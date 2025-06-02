2b09fb961be2bd3113d039153921a7db909d02dd
arvind021 <ak4392916@gmail.com>
2025-06-02 17:16:13 +0530

    Update call.py

--- AviaxMusic/core/call.py
+++ AviaxMusic/core/call.py
@@ -541,7 +541,7 @@ class Call(PyTgCalls):
                     db[chat_id][0]["mystic"] = run
                     db[chat_id][0]["markup"] = "stream"
 
-        async def ping(self):
+     async def ping(self):
         pings = []
         if config.STRING1:
             pings.append(await self.one.ping)
